1fd6eb5d50cb6667ed8569026848a069493ccf4f
TVoc <thomasvochten@hotmail.com>
2014-11-04 13:13:51 +0100

    Initial commit

---
+++ distri_assignment2/CarRental-lib/src/rental/CarType.java
@@ -0,0 +1,103 @@
+package rental;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CarType implements Serializable {    
+    //--------------------------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------------------------
+    // Java ee entity convention.
+    /**
+     * Create a new instance of CarType
+     */
+    public CarType() {
+    }
+    
+    public CarType(String name, 
+                   int nbOfSeats, 
+                   float trunkSpace, 
+                   double rentalPricePerDay, 
+                   boolean smokingAllowed) {
+        this.name = name;
+        this.nbOfSeats = nbOfSeats;
+        this.trunkSpace = trunkSpace;
+        this.rentalPricePerDay = rentalPricePerDay;
+        this.smokingAllowed = smokingAllowed;
+    }
+
+    //--------------------------------------------------------------------------
+    // Properties
+    //--------------------------------------------------------------------------
+    @Id
+    public String getName() {
+    	return this.name;
+    }
+   
+    private String name;
+
+    //--------------------------------------------------------------------------
+    public int getNbOfSeats() {
+        return nbOfSeats;
+    }
+    
+    private int nbOfSeats;
+    
+    //--------------------------------------------------------------------------
+    public boolean isSmokingAllowed() {
+        return smokingAllowed;
+    }
+    
+    private boolean smokingAllowed;
+
+    //--------------------------------------------------------------------------
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+    
+    private double rentalPricePerDay;
+    
+    //--------------------------------------------------------------------------
+    public float getTrunkSpace() {
+    	return trunkSpace;
+    }
+    
+    //trunk space in liters
+    private float trunkSpace;
+    
+    //--------------------------------------------------------------------------
+    // Class methods
+    //--------------------------------------------------------------------------
+    @Override
+    public String toString() {
+    	return String.format("Car type: %s \t[seats: %d, price: %.2f, smoking: %b, trunk: %.0fl]" , 
+                getName(), getNbOfSeats(), getRentalPricePerDay(), isSmokingAllowed(), getTrunkSpace());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+	int result = 1;
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+	if (obj == null)
+            return false;
+	if (getClass() != obj.getClass())
+            return false;
+	CarType other = (CarType) obj;
+	if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+	return true;
+    }
+}
---
+++ distri_assignment2/CarRental-lib/src/rental/Reservation.java
@@ -0,0 +1,50 @@
+package rental;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Reservation extends Quote {
+    private int carId;
+    
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+    public Reservation() {
+        super();
+    }
+    
+    public Reservation(Quote quote, int carId) {
+    	super(quote.getCarRenter(), quote.getStartDate(), quote.getEndDate(), 
+    		quote.getRentalCompany(), quote.getCarType(), quote.getRentalPrice());
+        this.carId = carId;
+    }
+    
+    /******
+     * ID *
+     ******/
+    
+    public int getCarId() {
+    	return carId;
+    }
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+    
+    private long id;
+    
+    /*************
+     * TO STRING *
+     *************/
+    
+    @Override
+    public String toString() {
+        return String.format("Reservation for %s from %s to %s at %s\nCar type: %s\tCar: %s\nTotal price: %.2f", 
+                getCarRenter(), getStartDate(), getEndDate(), getRentalCompany(), getCarType(), getCarId(), getRentalPrice());
+    }	
+}
